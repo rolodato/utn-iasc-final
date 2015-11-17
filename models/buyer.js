8b7ff560508e29e818a365432499c41359234f0a
Rodrigo López Dato <rodrigo@auth0.com>
2015-11-17 17:47:03 -0300

    Improve buyer validations

--- models/buyer.js
+++ models/buyer.js
@@ -2,12 +2,14 @@ module.exports = function(sequelize, Types) {
   return sequelize.define('Buyer', {
     name: {
       type: Types.STRING,
+      allowNull: false,
       validate: {
         notEmpty: true
       }
     },
     callbackUrl: {
       type: Types.STRING,
+      allowNull: false,
       unique: true,
       validate: {
         isUrl: true
