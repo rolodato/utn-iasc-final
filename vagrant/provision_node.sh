bc276a336625b56d1b860f6f6c3c34a5ff704124
Rodrigo López Dato <rodrigo@auth0.com>
2015-11-27 14:59:21 -0300

    Fix Windows npm i issues

--- vagrant/provision_node.sh
+++ vagrant/provision_node.sh
@@ -3,6 +3,6 @@ set -e -x
 
 sudo sed -i 's/127.0.0.1 localhost/192.168.50.1 localhost/' /etc/hosts
 curl -sL https://deb.nodesource.com/setup_5.x | bash -
-apt-get install -y nodejs libpq-dev
+apt-get install -y nodejs libpq-dev build-essential
 cd /vagrant
 npm install
